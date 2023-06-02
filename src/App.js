1885a1732ddca1a09bd8ecf10f7651b01783758f
Rafaela Gomes <111985355+rafaelagsz@users.noreply.github.com>
2023-06-02 10:11:15 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import style from "./style.css"
+
+function App() {
+  return (
+    <>
+    <div class="tela-inteira">
+      <header>
+        <h1>UniquePlay</h1>
+
+        <menu>
+          <li><button onClick="Click">Como Funciona?</button></li>
+          <li><button onClick="Click">Desenvolvedora</button></li>
+          <li><button onClick="Click">Constatos</button></li>
+        </menu>
+
+        <button onClick="Click" id="btnBusca">LogIn</button>
+      </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
